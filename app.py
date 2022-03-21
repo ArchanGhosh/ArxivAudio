6c25a31cfb38fd4078ae1a5732d3b67838a604b3
madhurima99 <rmadhurimaa99@gmail.com>
2022-03-21 19:05:57 +0530

    vanilla templates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config["DEBUG"] = True
 
 @app.route("/", methods=['GET'])
 def get_index():
-    return render_template("index.html", flag=0)
+    return render_template("index.html")
 
 
 @app.route("/papers", methods=['GET', 'POST'])
@@ -17,7 +17,24 @@ def get_papers():
     query = request.form['query']
     print(query)
     temp = ["paper1", "paper2", "paper3"]
-    return render_template("index.html", qry=query, lst=temp, flag=1)
+
+    return render_template("second.html", qry=query, paperlst=temp)
+
+
+@app.route("/paperselect", methods=['GET', 'POST'])
+def get_details():
+    pname = request.form['papername']
+    lmt = 10
+    pgs = [i+1 for i in range(lmt)]
+    return render_template("third.html", pname=pname, pgs=pgs)
+
+
+@app.route("/audio", methods=['GET', 'POST'])
+def download_audio():
+    start = request.form['start']
+    end = request.form['end']
+    print(start)
+    print(end)
 
 
 if __name__ == '__main__':
